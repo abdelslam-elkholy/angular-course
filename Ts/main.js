198e0cd6dd02798f7f6422235145d1380c8dc6ce
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-07-20 04:28:08 +0300

    update

---
+++ Ts/main.js
@@ -0,0 +1,9 @@
+// const ColorRed = 0
+// const ColorGreen = 1
+// const ColorBlue = 2
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
---
+++ Ts/main.ts
@@ -0,0 +1,9 @@
+// const ColorRed = 0
+// const ColorGreen = 1
+// const ColorBlue = 2
+
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
